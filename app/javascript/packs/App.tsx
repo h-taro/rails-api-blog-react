9cec916c6a88346d008102d3f3fff91eabc54d60
平石太郎 <taro.hiraishi@play.jp>
2023-04-29 14:44:07 +0900

    APIからデータを取得する

--- app/javascript/packs/App.tsx
+++ app/javascript/packs/App.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react'
+import api from './api'
+
+interface Post {
+    title: string
+}
 
 export default function App() {
+    const [posts, setPosts] = React.useState<Post[]>([])
+
+    React.useEffect(() => {
+        api.get('/posts')
+            .then(response => {
+                console.log(response.data)
+                setPosts(response.data)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }, [])
+
     return <h1>Hello, World!</h1>
 }
