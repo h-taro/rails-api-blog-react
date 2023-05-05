fba0897494c34f0de3b8182a46fb4852d4593758
平石太郎 <taro.hiraishi@play.jp>
2023-05-05 20:13:22 +0900

    ホーム画面のコンポーネントにスタイルを当てる、TODO: エディタのエラーを修正する

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.stylesheet_engine = :scss
   end
 end
